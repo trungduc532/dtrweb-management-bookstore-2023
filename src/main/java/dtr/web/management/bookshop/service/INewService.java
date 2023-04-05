d908a0036a548e158cc078d9417758a87a8a4b17
Trung Duc <trungduc532@gmail.com>
2023-04-05 23:57:05 +0700

    Update package dao and service

---
+++ src/main/java/dtr/web/management/bookshop/service/INewService.java
@@ -0,0 +1,5 @@
+package dtr.web.management.bookshop.service;
+
+public interface INewService {
+
+}
---
+++ src/main/java/dtr/web/management/bookshop/service/impl/NewService.java
@@ -0,0 +1,7 @@
+package dtr.web.management.bookshop.service.impl;
+
+import dtr.web.management.bookshop.service.INewService;
+
+public class NewService implements INewService{
+
+}
